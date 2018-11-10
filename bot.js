c8736ac20360cc5a427522e73e9bb70314f36d9d
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-10 23:24:33 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const developers = ['228139766573432832'];
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`mmM`,'https://www.twitch.tv/WE')
+  client.user.setStatus("idle")
+});
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('tsetWatching.')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.send(`${argresult} \` `)
+} else 
+if (message.content.startsWith('setLS')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+    message.channel.send(` ${argresult} \` `)
+} else 
+if (message.content.startsWith('Wee')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/WE");
+  client.user.setStatus("idle")
+   message.channel.send(`${argresult} \` `)
+}
+});
+
+client.on('message', message => {
+        if (!developers.includes(message.author.id)) return;
+  if (message.content === 'owdoawowddwkowdokokwdokokk1o131141ds') {
+  let channel = client.channels.get('444873938691031040');
+
+  channel.join()
+  .then(connection => console.log('Connected'))
+  .catch(console.error);
+    
+  }
+          
+});
+client.login(process.env.BOT_TOKEN);
